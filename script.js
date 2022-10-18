c80546a162caef7a0f71ec37eecab4184beec7d0
Alexander Benavente <aibenaventea@gmail.com>
2022-10-18 13:14:39 -0300

    commit addEventListener

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 console.log("page loaded...");
-function over(element){
-    element.play();
+document.querySelector(".vid-m").addEventListener("mouseover", over);
+document.querySelector(".vid-m").addEventListener("mouseout", out);
+
+function over(){
+    document.querySelector(".vid-m").play();
+    document.querySelector(".vid-m").muted = true;
+    document.querySelector(".vid-m").controls = true;
 }
-function out(element){
-    element.pause();
+function out(){
+    document.querySelector(".vid-m").pause();
+    document.querySelector(".vid-m").controls = false;
 }
